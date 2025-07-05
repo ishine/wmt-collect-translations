e1b75035d8a64f02c428b356d63778591bad714c
Tom Kocmi <kocmi@cohere.com>
2025-07-05 11:10:38 +0000

    added cache for gemini

--- tools/providers/gemini.py
+++ tools/providers/gemini.py
@@ -1,9 +1,22 @@
 import os
+import json
 import logging
 
 CLIENT = None
+gemini_cache = {}
 def lazy_get_client():
     global CLIENT
+    global gemini_cache
+
+    # for WMT25 to speed up, we used a separate script which didn't have backup mechanisms. Thus after collecting the translations, we fill the gaps with original script
+    if len(gemini_cache) == 0:
+        with open("gemini_cache.jsonl", "r") as f:
+            for line in f:
+                entry = json.loads(line)
+                if "response" in entry and "candidates" in entry['response']:
+                    if entry['response']['candidates'][0]['finishReason'] == "STOP":
+                        gemini_cache[entry["key"]] = entry['response']
+
     
     if CLIENT is None:
         from google import genai
@@ -23,37 +36,27 @@ def process_with_gemma_3_27b(request, temperature=0.0):
     return translate_with_gemini(request, "gemma-3-27b-it", max_tokens=32768, temperature=temperature)
 
 
-# for WMT25 to speed up, we used a separate script which didn't have backup mechanisms. Thus after collecting the translations, we fill the gaps with original script
-import json
-gemini_cache = {}
-with open("gemini_cache.jsonl", "r") as f:
-    for line in f:
-        entry = json.loads(line)
-        if "response" in entry and "candidates" in entry['response']:
-            if entry['response']['candidates'][0]['finishReason'] == "STOP":
-                gemini_cache[entry["key"]] = entry['response']
-
 
 def translate_with_gemini(request, model, max_tokens, temperature=0.0):
     client = lazy_get_client()
     from google.genai import types
 
+    if len(gemini_cache) > 0:
+        import hashlib
+        if "doc_id" in request:
+            hashid = f"{request['doc_id']}_{request['source_language']}_{request['target_language']}_{request['segment']}_{request['prompt_instruction']}"
 
-    import hashlib
-    if "doc_id" in request:
-        hashid = f"{request['doc_id']}_{request['source_language']}_{request['target_language']}_{request['segment']}_{request['prompt_instruction']}"
-
-    else:
-        hashid = f"{request['taskid']}_{request['prompt']}"
-    hashid = hashlib.md5(hashid.encode("utf-8")).hexdigest()
-    if hashid in gemini_cache:
-        result = gemini_cache[hashid]
-        input_tokens = result['usageMetadata']['promptTokenCount']
-        candidate_tokens = result['usageMetadata']['candidatesTokenCount']
-        thinking_tokens = result['usageMetadata']['thoughtsTokenCount']
-        return result['candidates'][0]['content']['parts'][0]['text'], {"input_tokens": input_tokens,
-                           "output_tokens": candidate_tokens + thinking_tokens,
-                           "thinking_tokens": thinking_tokens}
+        else:
+            hashid = f"{request['taskid']}_{request['prompt']}"
+        hashid = hashlib.md5(hashid.encode("utf-8")).hexdigest()
+        if hashid in gemini_cache:
+            result = gemini_cache[hashid]
+            input_tokens = result['usageMetadata']['promptTokenCount']
+            candidate_tokens = result['usageMetadata']['candidatesTokenCount']
+            thinking_tokens = result['usageMetadata']['thoughtsTokenCount']
+            return result['candidates'][0]['content']['parts'][0]['text'], {"input_tokens": input_tokens,
+                            "output_tokens": candidate_tokens + thinking_tokens,
+                            "thinking_tokens": thinking_tokens}
 
     
     config = types.GenerateContentConfig(
@@ -81,11 +84,12 @@ def translate_with_gemini(request, model, max_tokens, temperature=0.0):
         logging.warning(f"Skipping: {e}")
         return None
 
+    if response.candidates is None:
+        return None
+
     if response.candidates[0].finish_reason != "STOP":
         logging.warning(f"Finish reason: {response.candidates[0].finish_reason}; {response.text}")
         return None
-
-    assert response.candidates[0].finish_reason == "STOP", f"Finish reason: {response.choices[0].finish_reason}"
 
 
     input_tokens = response.usage_metadata.prompt_token_count
