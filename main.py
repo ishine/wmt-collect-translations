2e361b7f42443342fd5d901508d300b797251b42
Tom Kocmi <kocmi@cohere.com>
2025-07-10 23:29:43 +0000

    adding a parallel option for shuffling

--- main.py
+++ main.py
@@ -11,22 +11,33 @@ from tools.utils import collect_answers, SYSTEMS
  
 
 flags.DEFINE_enum('system', 'CommandA', SYSTEMS, 'Define the system to use for translation')
+flags.DEFINE_bool('parallel', False, 'Run in parallel mode (default: False)')
 
 FLAGS = flags.FLAGS
 
 def main(args):
     assert os.path.exists("genmt_blindset_wmt25.jsonl"), "Download genmt_blindset_wmt25.jsonl file from WMT website"
     blindset = pd.read_json("genmt_blindset_wmt25.jsonl", lines=True)
+    if FLAGS.parallel:
+        # avoid clashes by shuffling samples
+        blindset = blindset.sample(frac=1, random_state=42).reset_index(drop=True)
 
     answers = collect_answers(blindset, FLAGS.system)
     df = pd.DataFrame(answers)
 
+    # for each tgt_lang, count how many "FAILED" there are and if more than 25% are FAILED, remove that tgt_lang
+    for tgt_lang in df['tgt_lang'].unique():
+        num_none = df[df['tgt_lang'] == tgt_lang]['hypothesis'].str.contains("FAILED", na=False).sum()
+        if num_none > 0.25 * len(df[df['tgt_lang'] == tgt_lang]):
+            df = df[df['tgt_lang'] != tgt_lang]
+
     os.makedirs("wmt_translations", exist_ok=True)
     df.to_json(f"wmt_translations/{FLAGS.system}.jsonl", orient='records', lines=True, force_ascii=False)
 
-
     assert os.path.exists("blindset_mist_2025.json"), "Download blindset_mist_2025.json file from WMT website"
     blindset = pd.read_json("blindset_mist_2025.json")
+    if FLAGS.parallel:
+        blindset = blindset.sample(frac=1, random_state=42).reset_index(drop=True)
 
     answers = collect_answers(blindset, FLAGS.system, "mist")
     if answers is not None:
@@ -41,6 +52,7 @@ def main(args):
 
     mt_num_none = df[df['hypothesis'].str.contains("FAILED", na=False)]['hypothesis'].count()
     print(f"Number of untranslated answers in MT: {mt_num_none}")
+
 
 if __name__ == '__main__':
     app.run(main)
